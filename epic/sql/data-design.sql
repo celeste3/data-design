ab55b909466af303f42ae8077febef86f17b91f6
Celeste Whitaker <abqcc3@gmail.com>
2020-01-23 06:20:00 -0700

    Push

--- epic/sql/data-design.sql
+++ epic/sql/data-design.sql
@@ -4,5 +4,27 @@ CREATE TABLE profile (
 	shopAcivationToken char(35),
 	shopHash CHAR(99),
 	shopName VARCHAR(35),
+	UNIQUE (shopId),
+	UNIQUE (shopEmail),
 	primary key (shopId)
+);
+
+CREATE TABLE product(
+   productId,
+   productQuantity,
+   productPrice,
+   produceName,
+);
+
+CREATE TABLE customer(
+   customerId,
+   customerFirstname,
+   customerLastname,
+   customerStreet,
+   customerCity,
+   customerState,
+   customerZip,
+   customerActivation,
+   customerHash,
+   primary key (customerId)
 );
