31a580b1e59bf70994f8b08c5825e6aaeb3b5b1b
Celeste Whitaker <abqcc3@gmail.com>
2020-01-28 08:31:04 -0700

    Added DROP TABLE and cleaned up code

--- epic/sql/data-design.sql
+++ epic/sql/data-design.sql
@@ -1,30 +1,36 @@
+DROP TABLE  IF EXISTS customer;
+DROP TABLE  IF EXISTS product;
+DROP TABLE  IF EXISTS shop;
+
 CREATE TABLE shop (
-	shopId BINARY(25),
-	shopEmail VARCHAR(128),
-	shopAcivationToken char(35),
-	shopHash CHAR(99),
-	shopName VARCHAR(35),
+	shopId BINARY(16) NOT NULL,
+	shopEmail VARCHAR(128) NOT NULL,
+	shopAcivationToken CHAR(35),
+	shopHash CHAR(99) NOT NULL,
+	shopName VARCHAR(35) NOT NULL,
 	UNIQUE (shopId),
 	UNIQUE (shopEmail),
-	primary key (shopId)
+	PRIMARY KEY (shopId)
 );
 -- this a comment in SQL
 CREATE TABLE product(
-   productId BINARY,
-   productQuantity,
-   productPrice,
-   produceName,
+   productId BINARY (16) NOT NULL,
+   productQuantity SMALLINT NOT NULL,
+   productPrice SMALLINT NOT NULL,
+   produceName VARCHAR (124) NOT NULL,
+	FOREIGN KEY (productId) REFERENCES shop(shopId),
+	PRIMARY KEY (productId)
 );
 
 CREATE TABLE customer(
-   customerId BINARY,
-   customerFirstname VARCHAR(30),
-   customerLastname VARCHAR(30),
-   customerStreet VARCHAR(30),
-   customerCity VARCHAR(60),
-   customerState VARCHAR(30),
-   customerZip VARCHAR(15),
-   customerActivation CHAR (25),
-   customerHash CHAR (20),
-   primary key (customerId)
+   customerId BINARY (16) NOT NULL,
+   customerFirstname VARCHAR(30) NOT NULL,
+   customerLastname VARCHAR(30) NOT NULL,
+   customerStreet VARCHAR(30) NOT NULL,
+   customerCity VARCHAR(60) NOT NULL,
+   customerState VARCHAR(30) NOT NULL,
+   customerZip VARCHAR(15) NOT NULL,
+   customerActivation VARCHAR (25),
+   customerHash VARCHAR (97) NOT NULL,
+   PRIMARY KEY (customerId)
 );
